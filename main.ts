fd23b274b80f607a7a7a77922aa1580369590ecb
carmenzazueta <110991084+carmenzazueta@users.noreply.github.com>
2022-09-04 11:36:42 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Confused)
+    basic.pause(1000)
 })
